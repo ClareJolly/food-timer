2f081a57cd33d64455211a00957d2639a28e7def
Clare Jolly <clarejollyuk@gmail.com>
2018-12-29 10:16:33 +0000

    Updates

---
+++ precache-manifest.18e0bf1817e1b08e4c1ffc506fed9d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efdddb0e4a22ddbf460c",
+    "url": "/food-timer/static/css/main.6a8c9797.chunk.css"
+  },
+  {
+    "revision": "efdddb0e4a22ddbf460c",
+    "url": "/food-timer/static/js/main.efdddb0e.chunk.js"
+  },
+  {
+    "revision": "f52cafce18e235cc8ec1",
+    "url": "/food-timer/static/js/1.f52cafce.chunk.js"
+  },
+  {
+    "revision": "27e0450505c2f6a759af",
+    "url": "/food-timer/static/js/runtime~main.27e04505.js"
+  },
+  {
+    "revision": "0368505d1c3146edd8de29f2a585951d",
+    "url": "/food-timer/index.html"
+  }
+];
